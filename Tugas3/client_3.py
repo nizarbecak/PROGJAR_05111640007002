85dc89cd1d822f608b0bb2113081ba29c6ff9826
nizarbecak <32842771+nizarbecak@users.noreply.github.com>
2020-03-08 21:20:00 +0700

    Tugas 3 Progjar

---
+++ Tugas3/client_3.py
@@ -0,0 +1,36 @@
+import logging
+import requests
+import os
+import threading
+
+def download_gambar(url=None):
+    if (url is None):
+        return False
+    ff = requests.get(url)
+    tipe = dict()
+    tipe['image/png']='png'
+    tipe['image/jpg']='jpg'
+    tipe['image/jpeg']='jpg'
+
+    content_type = ff.headers['Content-Type']
+    logging.warning(content_type)
+    if (content_type in list(tipe.keys())):
+        namafile = os.path.basename(url)
+        ekstensi = tipe[content_type]
+        logging.warning(f"writing {namafile}.{ekstensi}")
+        fp = open(f"{namafile}.{ekstensi}","wb")
+        fp.write(ff.content)
+        fp.close()
+    else:
+        return False
+
+if __name__=='__main__':
+    url = ['https://www.its.ac.id/wp-content/uploads/sites/2/2020/02/WhatsApp-Image-2020-02-12-at-16.02.13-1024x683.jpeg',
+           'https://images.unsplash.com/photo-1503023345310-bd7c1de61c7d?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&w=1000&q=80']
+    threads = []
+
+    for i in range(2):
+        t = threading.Thread(target=download_gambar(url[i]),args=(i,))
+        threads.append(t)
+    for thread in threads:
+        thread.start()
